eefa46f9ab02f402a9d1c60ee976653194ec5792
Nikheel Patel <nikheel.patel0007@gmail.com>
2021-07-04 11:28:43 +0530

    python script modified

--- python/mouseMove.py
+++ python/mouseMove.py
@@ -7,41 +7,57 @@ logger.setLevel(logging.DEBUG)
 
 fh = logging.FileHandler('mousemove.log')
 fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
-mtr = mouse.Controller()
-ktr = keyboard.Controller()
-try:
-        #mins = float(input().strip())
-        #mins = 0.5
-        mins = 3
-        mtr.position = (2,2)
-        logger.info('pointer moved to 2, 2')
-        while True:
-                sleep(60*mins)
+
+class MouseMove:
+    __slots__ = ('mouse_controller', 'keyboard_controller', 'interval')
+
+    def __init__(self, interval_in_minutes=3):
+        self.mouse_controller = mouse.Controller()
+        self.keyboard_controller = keyboard.Controller()
+        self.interval = interval_in_minutes
+
+    def movemouse_event(self, pos):
+        self.mouse_controller.position = pos
+
+    def shiftkey_event(self, flag=True):
+        if flag:
+            self.keyboard_controller.press(keyboard.Key.shift_r)
+            sleep(2)
+            self.keyboard_controller.release(keyboard.Key.shift_r)
+        else:
+            self.keyboard_controller.press(keyboard.Key.shift)
+            sleep(2)
+            self.keyboard_controller.release(keyboard.Key.shift)
+
+    def run(self):
+        try:
+            self.movemouse_event((2, 2))
+            logger.info('pointer moved to 2, 2')
+            while True:
+                sleep(60 * self.interval)
                 logger.info('sleeping')
                 for _ in range(0, 10):
-                        mtr.move(0,_*4)
-                        sleep(1)
+                    self.movemouse_event((0, _*4))
+                    sleep(1)
                 logger.info('movements made 10 times')
-                mtr.position = (2,2)
+                self.movemouse_event((2, 2))
                 logger.info('pointer moved to 2, 2')
-                ktr.press(keyboard.Key.shift_r)
-                sleep(2)
-                ktr.release(keyboard.Key.shift_r)
+                self.shiftkey_event()
                 logger.info('right shift pressed and released')
-                ktr.press(keyboard.Key.shift)
-                sleep(2)
-                ktr.release(keyboard.Key.shift)
+                self.shiftkey_event(False)
                 logger.info('left shift pressed and released')
-                ktr.press(keyboard.Key.shift_r)
-                sleep(2)
-                ktr.release(keyboard.Key.shift_r)
+                self.shiftkey_event()
                 logger.info('right shift pressed and released')
-except :
-        logger.info('app exit invoked')
-        logger.info("Good bye Headshot")
-        ktr.release(keyboard.Key.shift)
-        ktr.release(keyboard.Key.shift_r)
-        print("Good bye Headshot")
+        except:
+            logger.info('app exit invoked')
+            logger.info("Good bye Headshot")
+            self.keyboard_controller.release(keyboard.Key.shift)
+            self.keyboard_controller.release(keyboard.Key.shift_r)
+            print("Good bye Headshot")
+
+task = MouseMove()
+task.run()
